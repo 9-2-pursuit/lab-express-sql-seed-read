b1fe17db1a12a484f5e756c2681bc4626e66cf1c
Tech-Kat <katpelle@pursuit.org>
2023-01-17 21:42:59 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// DEPENDENCIES
+const cors = require("cors");
+const express = require("express");
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json()); // tells app to send and receive json
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to Song Playlist App");
+});
+
+// Song ROUTES
+const songController = require("./controllers/songController.js");
+app.use("/songs", songController);
+
+// 404 PAGE
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
+
+// EXPORT
+module.exports = app; //allow you to use files in other components
---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+\c songs_dev;
+
+INSERT INTO Songs (name, artist, album, time, is_favorite) VALUES
+('What About Me', 'Nino Man', 'Mixed Emotions', "2:26", false),
+('As a Bad Gyal', 'Stalk Ashley', 'no album', "2:20", false),
+('How Deep', 'Tai Verdes', 'HDTV', "3:00", true);
