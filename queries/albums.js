cb0e081a8ad37ec94f927dd9fcceecd989e30d9d
Bo Latt <bolattny@gmail.com>
2023-01-23 05:25:49 -0500

    modify album query

--- queries/albums.js
+++ queries/albums.js
@@ -1,7 +1,7 @@
 const db = require("../db/dbConfig");
 
 async function getSongsFromAlbum(id) {
-  const songs = await db.any("SELECT * FROM songs WHERE id= $1", id);
+  const songs = await db.any("SELECT * FROM songs WHERE album_id= $1", id);
   return songs;
 }
 
