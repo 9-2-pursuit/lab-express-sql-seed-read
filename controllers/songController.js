29b4498272b9844db71ca905ea2ccde997491a59
i-guerrero <isaacguerrero@pursuit.org>
2023-01-30 11:50:31 -0500

    Add show and create routes

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs");
+const { getAllSongs, getSong, createSong } = require("../queries/songs");
+const {
+  checkName,
+  checkArtist,
+  checkBoolean,
+} = require("../validations/checkSongs");
 
 // INDEX
 songs.get("/", async (req, res) => {
@@ -9,6 +14,27 @@ songs.get("/", async (req, res) => {
     res.status(200).json(allSongs);
   } else {
     res.status(500).json({ error: "server error" });
+  }
+});
+
+// SHOW
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const song = await getSong(id);
+  if (song) {
+    res.status(200).json(song);
+  } else {
+    res.status(404).json({ error: "Page not found" });
+  }
+});
+
+// CREATE
+songs.post("/", checkName, checkArtist, checkBoolean, async (req, res) => {
+  try {
+    const song = await createSong(req.body);
+    res.json(song);
+  } catch (error) {
+    res.status(400).json({ error: error });
   }
 });
 
