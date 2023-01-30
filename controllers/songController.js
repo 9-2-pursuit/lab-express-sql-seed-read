96b32865023a40b50a6c1e8c2b806083b4824ddd
i-guerrero <isaacguerrero@pursuit.org>
2023-01-30 14:05:20 -0500

    add update and deletle routes

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs, getSong, createSong } = require("../queries/songs");
+const {
+  getAllSongs,
+  getSong,
+  createSong,
+  deleteSong,
+  updateSong,
+} = require("../queries/songs");
 const {
   checkName,
   checkArtist,
@@ -35,6 +41,28 @@ songs.post("/", checkName, checkArtist, checkBoolean, async (req, res) => {
     res.json(song);
   } catch (error) {
     res.status(400).json({ error: error });
+  }
+});
+
+// DELETE
+songs.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedSong = await deleteSong(id);
+  if (deletedSong.id) {
+    res.status(200).json(deletedSong);
+  } else {
+    res.status(404).json({ error: "Song not found" });
+  }
+});
+
+// UPDATE
+songs.put("/:id", checkName, checkArtist, checkBoolean, async (req, res) => {
+  const { id } = req.params;
+  const updatedSong = await updateSong(id, req.body);
+  if (updatedSong.id) {
+    res.status(200).json(updatedSong);
+  } else {
+    res.status(400).json({ error: "Could not update song" });
   }
 });
 
